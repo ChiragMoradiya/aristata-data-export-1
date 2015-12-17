98c23700c66c1e80d8087107a1cd828774f1cf8f
Ashvin <ashvin.kanani@dreamworldsolutions.com>
2015-12-16 22:25:27 -0800

    initial commit

---
+++ src/com/dw/aristata/test1/TestServlet.java
@@ -0,0 +1,43 @@
+package com.dw.aristata.test1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.microsoft.schemas.sharepoint.soap.ListsStub;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ListsStub listsStub = new ListsStub();
+		  String sResponse = listsStub.getListCollection(new ListsStub.GetListCollection()).toString();
+		  response.getWriter().append(sResponse);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
